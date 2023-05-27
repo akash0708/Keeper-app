0d20a349d289881c51774e995876f73d3612b2c1
Akash Bag <110753356+akash0708@users.noreply.github.com>
2023-05-27 08:21:16 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <p className='footer-text'>Copyright &#64; 2023</p>
+    </div>
+  )
+}
+
+// make it dynamic
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Note() {
+  return (
+    <div className='note'>
+      <h1 className='note-title'>This is the title</h1>
+      <p className='note-content'>this is the content</p>
+    </div>
+  )
+}
